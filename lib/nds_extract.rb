bdcc5aa281e39628886553ee1e246d897a9c8420
Mitchell T Alton <mitchell.alton@live.com>
2020-01-20 16:07:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,9 +1,33 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 require 'pp'
-def pretty_print(nds)
-  pp nds 
-end
+
+directors_database
+=> [{:name=>"Stephen Spielberg",
+  :movies=>
+   [{:title=>"Jaws",
+     :studio=>"Universal",
+     :worldwide_gross=>260000000,
+     :release_year=>1975},
+    {:title=>"Close Encounters of the Third Kind",
+     :studio=>"Columbia",
+     :worldwide_gross=>135189114,
+     :release_year=>1977},
+    {:title=>"Raiders of the Lost Ark",
+     :studio=>"Paramount",
+     :worldwide_gross=>248159971,
+     :release_year=>1981},
+    {:title=>"E.T. the Extra-terrestrial",
+     :studio=>"Universal",
+     :worldwide_gross=>435110554,
+     :release_year=>1982},
+    {:title=>"Schindler's List",
+     :studio=>"Universal",
+     :worldwide_gross=>96898818,
+     :release_year=>1993},
+    {:title=>"Lincoln",
+     :studio=>"Buena Vista",
+     
 
 def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
